b7178c5db5e98f5f6defa82c4ac7e4ff4907a86b
Jonadab <jingaespinoza@gmail.com>
2021-05-22 17:45:33 -0500

    segunda presentacion avance

---
+++ WebServices/conecta/funciones.php
@@ -0,0 +1,34 @@
+<?php 
+	function bd_conecta(){
+		    
+		// $servidor = "192.168.90.8:1433";
+		// $usuario = "sa";
+		// $clave = "Redes--";
+		// $bd = "bk_simi_01_05_2020";
+
+		// $servidor = "192.168.90.15:1433";
+		// $usuario = "acceso_produccion";
+		// $clave = "db-Pr0duccion2018-";
+		// $bd = "simi";
+
+		$servidor = "192.168.90.15:1433";
+		$usuario = "acceso_produccion";
+		$clave = "db-Pr0duccion2018-";
+		$bd = "SGA";
+		// $bd = "SGA_FEBRERO_02_2016";
+
+		$conn = mssql_connect($servidor,$usuario,$clave);
+		
+		if($conn)
+ 		{ 
+ 			$okbd = mssql_select_db($bd,$conn);	
+ 		}else
+ 		{
+			echo "Error de Conexion";
+   			exit(0);
+ 		}
+		
+		return $conn;
+
+	}
+?>
